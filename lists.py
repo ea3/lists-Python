a3487228c7c7c0c4b6a37a03684c36b82366d467
Emilio Araos <ea3dev@gmail.com>
2020-01-10 12:29:02 -0700

    concatenating lists

--- lists.py
+++ lists.py
@@ -1,2 +1,8 @@
 my_list = [1,2,3]
 my_list = ['STRING', 100, 23.2]
+print(len(my_list)) # prints the length of the list. 
+mylist = ['one','two','three']
+print(mylist[0]) # prints elemtent at position 0.
+print(mylist[1:]) # prints elements 1 and two from the list. 
+another_list = ['four', 'five']
+print(mylist + another_list)
