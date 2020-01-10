712366f1ae63c6cfcfe3f65f94b5b0ab24f016f1
Emilio Araos <ea3dev@gmail.com>
2020-01-10 12:34:57 -0700

    indexing, slicing and append() method

--- lists.py
+++ lists.py
@@ -1,3 +1,5 @@
+# Indexing , slicing and concatenation of lists. 
+
 my_list = [1,2,3]
 my_list = ['STRING', 100, 23.2]
 print(len(my_list)) # prints the length of the list. 
@@ -5,4 +7,13 @@ mylist = ['one','two','three']
 print(mylist[0]) # prints elemtent at position 0.
 print(mylist[1:]) # prints elements 1 and two from the list. 
 another_list = ['four', 'five']
-print(mylist + another_list)
+newlist = mylist + another_list
+print(newlist)
+newlist[0] = 'ONE IN ALL CAPS'
+print(newlist)
+newlist.append('six')
+print(newlist)
+newlist.append('seven')
+newlist.append(8)
+newlist.append("NINE")
+print(newlist)
